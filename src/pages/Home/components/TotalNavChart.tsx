b18786a14101504dbacb232ff026ce40ff559711
mehedi8430 <mehedialhasan1999@gmail.com>
2025-06-27 06:40:04 +0600

    feat: update TotalNavChart component to use total_nav data; enhance chart with YAxis and improved styling

--- src/pages/Home/components/TotalNavChart.tsx
+++ src/pages/Home/components/TotalNavChart.tsx
@@ -1,5 +1,4 @@
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
-
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
 import {
   type ChartConfig,
   ChartContainer,
@@ -8,56 +7,75 @@ import {
 } from "@/components/ui/chart";
 
 const chartData = [
-  { month: "Mon", desktop: 186 },
-  { month: "Tue", desktop: 305 },
-  { month: "Wed", desktop: 237 },
-  { month: "Thu", desktop: 73 },
-  { month: "Fri", desktop: 209 },
-  { month: "Sat", desktop: 214 },
-  { month: "Sun", desktop: 214 },
-  { month: "Mon", desktop: 214 },
+  { date: "2025-03-17", total_nav: 800 },
+  { date: "2025-03-18", total_nav: 810 },
+  { date: "2025-03-19", total_nav: 820 },
+  { date: "2025-03-20", total_nav: 830 },
+  { date: "2025-03-21", total_nav: 840 },
+  { date: "2025-03-22", total_nav: 850 },
+  { date: "2025-03-23", total_nav: 860 },
+  { date: "2025-03-24", total_nav: 870 },
 ];
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "var(--chart-5)",
+  total_nav: {
+    label: "Total Nav",
+    color: "var(--color-chart-1)",
   },
 } satisfies ChartConfig;
 
 export default function TotalNavChart() {
   return (
-    <div>
-      <ChartContainer config={chartConfig}>
-        <AreaChart
-          accessibilityLayer
-          data={chartData}
-          margin={{
-            left: 12,
-            right: 12,
+    <ChartContainer
+      config={chartConfig}
+      className="min-h-[200px] w-full"
+      style={{ height: 300 }}
+    >
+      <AreaChart
+        accessibilityLayer
+        data={chartData}
+        margin={{
+          left: 12,
+          right: 12,
+        }}
+      >
+        {/* <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#2D9C74" stopOpacity={1.2} />
+            <stop offset="95%" stopColor="#9DCCBC" stopOpacity={0.2} />
+          </linearGradient>
+        </defs> */}
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="date"
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(value) => {
+            const date = new Date(value);
+            return date.toLocaleDateString("en-US", { weekday: "short" });
           }}
-        >
-          <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="month"
-            tickLine={false}
-            axisLine={false}
-            tickMargin={8}
-            tickFormatter={(value) => value.slice(0, 3)}
-          />
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent indicator="line" />}
-          />
-          <Area
-            dataKey="desktop"
-            type="natural"
-            fill="var(--color-desktop)"
-            fillOpacity={0.4}
-            stroke="var(--color-desktop)"
-          />
-        </AreaChart>
-      </ChartContainer>
-    </div>
+        />
+        <YAxis
+          ticks={[800, 810, 820, 830, 840, 850, 860, 870]}
+          domain={[800, 870]}
+          axisLine={false}
+          tickLine={false}
+          tick={{ fill: "var(--color-chart-1-foreground)" }}
+          tickFormatter={(value) => `$${value.toFixed(1)}`}
+        />
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent indicator="dot" />}
+        />
+        <Area
+          dataKey="total_nav"
+          type="monotone"
+          fill="var(--color-chart-1)"
+          fillOpacity={0.4}
+          stroke="var(--color-chart-1)"
+        />
+      </AreaChart>
+    </ChartContainer>
   );
 }
