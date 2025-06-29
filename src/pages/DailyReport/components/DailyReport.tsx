292ef9481cea5a2019cf800ce059d99a60c8c637
Sheikh-Redoan <sheikhredoan99@gmail.com>
2025-06-29 06:21:56 +0600

    fix: format code for consistency and improve readability in Accordion and DailyReport components

--- src/pages/DailyReport/components/DailyReport.tsx
+++ src/pages/DailyReport/components/DailyReport.tsx
@@ -27,72 +27,72 @@ export default function DailyReport() {
         <section className="flex flex-col gap-4">
             <Accordion type="single" collapsible className="w-full">
                 <ScrollArea className="h-[85vh]">
-{performanceReportCards.map((report, index) => (
-                    <AccordionItem
-                        value={`item-${index + 1}`}
-                        key={index}
-                        className="border-b-0 mb-5"
-                    >
-                        <div className="rounded-lg border shadow-sm ">
-                            <div className="bg-card text-card-foreground flex items-center justify-between rounded-lg px-5 py-[18px] shadow-sm w-full flex-wrap">
+                    {performanceReportCards.map((report, index) => (
+                        <AccordionItem
+                            value={`item-${index + 1}`}
+                            key={index}
+                            className="border-b-0 mb-5"
+                        >
+                            <div className="rounded-lg border shadow-sm ">
+                                <div className="bg-card text-card-foreground flex items-center justify-between rounded-lg px-5 py-[18px] shadow-sm w-full flex-wrap">
 
 
-                                {/* Left Section */}
-                                <div className="flex items-center gap-4">
-                                    <div className="flex items-center gap-2 text-muted-foreground">
-                                        <AccordionTrigger className="hover:no-underline" />
+                                    {/* Left Section */}
+                                    <div className="flex items-center gap-4">
+                                        <div className="flex items-center gap-2 text-muted-foreground">
+                                            <AccordionTrigger className="hover:no-underline" />
+                                        </div>
+                                        <CalendarDays className="h-6 w-6 text-muted-foreground" />
+                                        <div>
+                                            <p className="font-medium text-foreground">
+                                                {report.date}
+                                            </p>
+                                            <p className="text-xs text-muted-foreground">
+                                                Daily Performance Report
+                                            </p>
+                                        </div>
                                     </div>
-                                    <CalendarDays className="h-6 w-6 text-muted-foreground" />
-                                    <div>
-                                        <p className="font-medium text-foreground">
-                                            {report.date}
-                                        </p>
-                                        <p className="text-xs text-muted-foreground">
-                                            Daily Performance Report
-                                        </p>
+
+                                    {/* Right Section */}
+                                    <div className="flex items-center gap-30">
+                                        <div>
+                                            <p className="text-xs text-muted-foreground">
+                                                STARTING NAV
+                                            </p>
+                                            <p className="font-medium text-foreground">
+                                                {report.startingNAV}
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <p className="text-xs text-muted-foreground">ENDING NAV</p>
+                                            <p className="font-medium text-foreground">
+                                                {report.endingNAV}
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <p className="text-xs text-muted-foreground text-right">
+                                                GROWTH RATE
+                                            </p>
+                                            <p
+                                                className={`font-medium text-right ${report.growthRate.sign === "+"
+                                                    ? "text-green-500"
+                                                    : "text-red-500"
+                                                    }`}
+                                            >
+                                                {report.growthRate.sign}
+                                                {report.growthRate.value}%
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-
-                                {/* Right Section */}
-                                <div className="flex items-center gap-30">
-                                    <div>
-                                        <p className="text-xs text-muted-foreground">
-                                            STARTING NAV
-                                        </p>
-                                        <p className="font-medium text-foreground">
-                                            {report.startingNAV}
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className="text-xs text-muted-foreground">ENDING NAV</p>
-                                        <p className="font-medium text-foreground">
-                                            {report.endingNAV}
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className="text-xs text-muted-foreground text-right">
-                                            GROWTH RATE
-                                        </p>
-                                        <p
-                                            className={`font-medium text-right ${report.growthRate.sign === "+"
-                                                ? "text-green-500"
-                                                : "text-red-500"
-                                                }`}
-                                        >
-                                            {report.growthRate.sign}
-                                            {report.growthRate.value}%
-                                        </p>
-                                    </div>
-                                </div>
+                                <AccordionContent className="bg-card text-card-foreground border-t rounded-b-lg py-4 px-10">
+                                    <p>{report.deception}</p>
+                                </AccordionContent>
                             </div>
-                            <AccordionContent className="bg-card text-card-foreground border-t rounded-b-lg py-4 px-10">
-                                <p>{report.deception}</p>
-                            </AccordionContent>
-                        </div>
-                    </AccordionItem>
-                ))}
+                        </AccordionItem>
+                    ))}
                 </ScrollArea>
-                
+
             </Accordion>
         </section>
     );
--- src/pages/DailyReport/index.tsx
+++ src/pages/DailyReport/index.tsx
@@ -9,7 +9,7 @@ export default function DailyReportPage() {
                 <h3 className="justify-start text-(--praimary) text-lg font-semibold font-Inter mb-1 ">Daily Report</h3>
                 <p className="justify-start text-(--muted-foreground) text-sm font-medium font-Inter ">Comprehensive daily performance analysis and market commentary</p>
             </div>
-            <DailyReport/>
+            <DailyReport />
         </section>
     );
 }
